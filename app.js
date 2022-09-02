7b33787a30979cc298ce5a1f5105e9782d1baecb
Daniel Davies <dan.davies@oak.com>
2022-09-02 15:43:47 +0100

    ✨ Initial Express skeleton

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello my jubblies! ${process.env.API_KEY}`);
 });
 
 app.listen(port, () => {
